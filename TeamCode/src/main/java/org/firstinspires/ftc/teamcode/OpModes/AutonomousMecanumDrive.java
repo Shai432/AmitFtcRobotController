d32eabe3e700bf7c51517f8af3ffcc7066db4ff6
Avital <shai.avital@intel.com>
2024-02-08 22:34:37 +0200

    Fix driver consts, and more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutonomousMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutonomousMecanumDrive.java
@@ -6,12 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.ComponentCommands.AutoMecanumDrive;
-import org.firstinspires.ftc.teamcode.ComponentCommands.ClawMovement;
-import org.firstinspires.ftc.teamcode.ComponentCommands.ElevatorMovement;
-import org.firstinspires.ftc.teamcode.ComponentCommands.MecanumDrive;
-import org.firstinspires.ftc.teamcode.Components.Claw;
 import org.firstinspires.ftc.teamcode.Components.Elevator;
-import org.firstinspires.ftc.teamcode.Components.RobotMotors;
 import org.firstinspires.ftc.teamcode.RoadRunner.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.internals.OpModeBase;
 import org.firstinspires.ftc.teamcode.internals.TelemetryHandler;
@@ -55,15 +50,17 @@ public class AutonomousMecanumDrive extends OpModeBase {
         TelemetryHandler.logData("Status", "Run Time: " + runtime.toString());
         // follow the path:
         drive.followTrajectory(path);
-        this.motionRunner.Run();
+        // this.motionRunner.Run();
         TelemetryHandler.update();
     }
 
     @Override
     public void customInit() {
-        this.elv = new Elevator();
+        /*this.elv = new Elevator();
         this.motionRunner.Add(new MecanumDrive(new RobotMotors()).SetSource(this.gamepad1))
                 .Add(new ClawMovement(new Claw()).SetSource(this.gamepad1))
-                .Add(new ElevatorMovement(new Elevator()).SetSource(this.gamepad1));
+                .Add(new ElevatorMovement(new Elevator()).SetSource(this.gamepad1));*/
+
+        this.SetAutoPath();
     }
 }
